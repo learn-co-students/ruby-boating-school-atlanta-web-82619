ecc4399f24a058b68d3e0e394a14bc720d296716
github username <github email address>
2019-09-03 16:39:20 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,33 @@
 class Instructor
+    attr_reader :name
+    @@all = []
 
+    def initialize (name)
+        @name = name
+        @@all.push(self)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def pass_student (student, test_name)
+        test = BoatingTest.all.find{|test| (test.test_name == test_name) && (test.student == student)}
+        if test
+            test.test_status = 'passed'
+        else
+            test = BoatingTest.new(student, test_name, 'passed', self)
+        end
+        test
+    end
+
+    def fail_student (student, test_name)
+        test = BoatingTest.all.find{|test| (test.test_name == test_name) && (test.student == student)}
+        if test
+            test.test_status = 'failed'
+        else
+            test = BoatingTest.new(student, test_name, 'failed', self)
+        end
+        test
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,28 @@
 class Student
+    @@all = []
 
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test (test_name, test_status, instructor)
+        test = BoatingTest.new(self, test_name, test_status, instructor)
+    end
+
+    def self.find_student(first_name)
+        self.all.find{|student| student.first_name == first_name}
+    end
+
+    def tests
+        BoatingTest.all.select{|test| test.student == self}
+    end
+    
+    def grade_percentage
+        self.tests.reduce(0) {|memo, test| test.test_status == 'passed' ? memo + 1 : memo} / tests.size.to_f
+    end
 end
